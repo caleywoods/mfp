9cf32ae74650caf60ab287bf25bf3a92a6a2c325
Caley Woods <caley.w@gmail.com>
2018-05-07 21:48:45 -0500

    Drawing 4x4 Grid
    Using 2D rendering context with HTML5 canvas to draw a simple 4x4
    grid to begin trying to make a puzzle. This is very simple and
    probably wrong but at least there's something started and on the page.

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 const maze_canvas = document.querySelector('#maze');
+const context     = maze_canvas.getContext('2d');
+
+const wh = [100,100];
+
+context.strokeStyle = 'blue';
+
+// Column 1
+context.strokeRect(50,50,...wh);
+context.strokeRect(50,150, ...wh);
+context.strokeRect(50,250,...wh);
+context.strokeRect(50,350,...wh);
+
+// Column 2
+context.strokeRect(150,50,...wh);
+context.strokeRect(150,150, ...wh);
+context.strokeRect(150,250, ...wh);
+context.strokeRect(150,350, ...wh);
+
+
+// Column 3
+context.strokeRect(250,50,...wh);
+context.strokeRect(250,150,...wh);
+context.strokeRect(250,250,...wh);
+context.strokeRect(250,350,...wh);
+
+// Column 4
+context.strokeRect(350,50,...wh);
+context.strokeRect(350,150,...wh);
+context.strokeRect(350,250,...wh);
+context.strokeRect(350,350,...wh);
 
 console.log(maze_canvas);
+console.log(context);
