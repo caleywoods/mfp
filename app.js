bc39190b85f23fc409c5f568c98ed864e50739d5
Caley Woods <caley@vcreativeinc.com>
2019-10-14 20:47:03 -0500

    Now procedurally drawing grid

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
 import Grid from './classes/Grid.js';
 
-
-const maze_canvas = document.querySelector('#maze');
-const context = maze_canvas.getContext('2d');
-
-const wh = [100,100];
-
 const maze = new Grid(4,4);
+maze.setStart(4,1);
+console.log(maze);
 
-context.strokeStyle = 'blue';
-context.lineWidth = 5;
-
-// Column 1
-context.strokeRect(50,50,...wh);
-context.strokeRect(50,150, ...wh);
-context.strokeRect(50,250,...wh);
-context.strokeRect(50,350,...wh);
-
-// Column 2
-context.strokeRect(150,50,...wh);
-context.strokeRect(150,150, ...wh);
-context.strokeRect(150,250, ...wh);
-context.strokeRect(150,350, ...wh);
-
-// Column 3
-context.strokeRect(250,50,...wh);
-context.strokeRect(250,150,...wh);
-context.strokeRect(250,250,...wh);
-context.strokeRect(250,350,...wh);
-
-// Column 4
-context.strokeRect(350,50,...wh);
-context.strokeRect(350,150,...wh);
-context.strokeRect(350,250,...wh);
-context.strokeRect(350,350,...wh);
-
-console.log(maze_canvas);
-console.log(context);
+maze.draw();
--- classes/Grid.js
+++ classes/Grid.js
@@ -16,11 +16,52 @@ export default class Grid {
                 this.nodes[row][col] = node;
             }
         }
-        console.log('Grid has been constructed: ', this.nodes);
     }
 
     setStart(row, col) {
-        const node = this.nodes[row][col];
+        // Subtract 1 fron row and col to allow "human" input, meaning first row is 1, etc
+        const node = this.nodes[row - 1][col - 1];
         node.isStart = true;
+    }
+
+    setEnd(row, col) {
+        // Subtract 1 fron row and col to allow "human" input, meaning first row is 1, etc
+        const node = this.nodes[row - 1][col - 1];
+        node.isEnd = true;
+    }
+
+    // Actually draw our grid to the screen using Canvas API
+    draw() {
+        const maze_canvas = document.querySelector('#maze');
+        const context = maze_canvas.getContext('2d');
+        // width and height of our grid squares
+        const wh = [100, 100];
+        context.strokeStyle = 'blue';
+        context.lineWidth = 5;
+        let x = 50;
+        for (let row = 0; row < this.rows; row++) {
+
+            if (row > 0) {
+                x += 100;
+            }
+            let y = 50;
+            for (let col = 0; col < this.cols; col++) {
+
+                if (col > 0) {
+                    y += 100;
+                }
+
+                const node = this.nodes[row][col];
+                // if (node.isStart) {
+                //     context.fillStyle = 'green';
+                //     context.fillRect(x + (context.lineWidth / 2), y + (context.lineWidth / 2), 100 - context.lineWidth, 100 - context.lineWidth);
+                // } else if (node.isEnd) {
+                //     context.fillStyle = 'red';
+                    // context.fillRect(x, y, ...wh);
+                // } else {
+                    context.strokeRect(x, y, ...wh);
+                // }
+            }
+        }
     }
 }
