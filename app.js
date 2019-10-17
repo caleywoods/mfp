9a3410039779d14ed68c7d024d9b1c81ef45943a
Caley Woods <caley@vcreativeinc.com>
2019-10-17 08:03:09 -0500

    Larger default grid

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import Grid from './classes/Grid.js';
 import BinaryTree from './algorithms/maze-generation/BinaryTree.js';
 
 const gridConfig = {
-    squareHeight: 50,
-    squareWidth: 50
+    squareHeight: 10,
+    squareWidth: 10
 };
 
 // const maze = new Grid(10, 10, gridConfig);
-const maze = new BinaryTree(10, 10, gridConfig);
+const maze = new BinaryTree(90, 100, gridConfig);
 // console.log(maze);
 
 // @TODO: Make these able to be set via mouse click
