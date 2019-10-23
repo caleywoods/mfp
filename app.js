0ba69432e71ade2f645e51c9adcc8bc5d963afed
Caley Woods <caley@vcreativeinc.com>
2019-10-23 00:16:45 -0500

    Save maze as image functionality

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ import Dijkstra from './algorithms/maze-traversal/Dijkstra.js';
 // Cell size in pixels
 const gridConfig = {
     showIDs: false,
-    squareHeight: 15,
-    squareWidth: 15
+    squareHeight: 30,
+    squareWidth: 30
 };
 
-const maze = new Sidewinder(40, 40, gridConfig);
+const maze = new Sidewinder(10, 10, gridConfig);
 
 const solverConfig = {
-    showWeightedScores: false
+    showWeightedScores: false,
+    slowMo: true
 };
 
 maze.create();
-const solver = new Dijkstra(maze, solverConfig);
+// const solver = new Dijkstra(maze, solverConfig);
+
+const saveAsImage = () => {
+    canvasToImage('maze', {
+        name: `Maze-${Math.floor(Math.random() * 1000)}`,
+        type: 'png',
+        quality: 1
+    });
+}
+
+const saveBtn = document.querySelector('#save-maze-btn');
+saveBtn.addEventListener('click', saveAsImage, false);
